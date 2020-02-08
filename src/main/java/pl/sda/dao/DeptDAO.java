f2d8973350cd484c9d2767b5028f6ca394c0398a
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-02-08 12:36:59 +0100

    JAVApoz20 group changes

--- src/main/java/pl/sda/dao/DeptDAO.java
+++ src/main/java/pl/sda/dao/DeptDAO.java
@@ -7,16 +7,9 @@ import java.util.List;
 /**
  * Created by pzawa on 02.02.2017.
  */
-public interface DeptDAO {
-    Department findById(int id) throws Exception;
-
-    void create(Department department) throws Exception;
-
-    void update(Department department) throws Exception;
+public interface DeptDAO extends EntityDAO<Department> {
 
     void updateName(int deptId, String dname) throws Exception;
-
-    void delete(int id) throws Exception;
 
     List<Department> findByName(String name);
 
